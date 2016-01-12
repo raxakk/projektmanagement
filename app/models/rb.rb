32add9961d0a1906e80d0cc690a9e0d9f8d6bc4f
Philipp Ringele <phi.ri@gmx.de>
2016-01-12 16:02:03 +0100

    Löschen funktioniert

--- app/models/rb.rb
+++ app/models/rb.rb
@@ -1,5 +1,5 @@
 class Rb < ActiveRecord::Base
   has_many :wb
-  has_many :Rbs, foreign_key: :parent_id, :dependent => :delete_all
+  has_many :Rbs, foreign_key: :parent_id, :dependent => :destroy
   belongs_to :Rb, foreign_key: :parent_id
 end
--- app/models/wb.rb
+++ app/models/wb.rb
@@ -1,6 +1,6 @@
 class Wb < ActiveRecord::Base
   belongs_to :worker
   belongs_to :rb
-  has_many :Wbs, foreign_key: :parent_id, :dependent => :delete_all
+  has_many :Wbs, foreign_key: :parent_id, :dependent => :destroy
   belongs_to :Wb, foreign_key: :parent_id
 end
