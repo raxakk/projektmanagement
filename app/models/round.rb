e8370df420d45b9e96f8800bbf1e36829f98f7a5
Marco Dierks <mdierks91@googlemail.com>
2016-01-26 16:23:05 +0100

    ram Fehlermeldungen und Button aktiv definiert. Rounds Validierung angelgt

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,9 @@
 class Round < ActiveRecord::Base
   has_many :forms, dependent: :delete_all
+  validates :member_number, numericality: {allow_blank: false, less_than_or_equal_to: 10}
 end
+  round = Round.new
+  round.valid? # => false
+  round.errors[:member_number]
+
+
